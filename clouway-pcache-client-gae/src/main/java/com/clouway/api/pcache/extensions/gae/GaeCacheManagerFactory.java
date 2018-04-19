04ff63ce000c8a410d05cb50abe818b1ca0125b6
Vasil <v.mitov.clouway@gmail.com>
2018-04-19 12:59:08 +0300

    pcache: add default error handler for CacheManager

--- clouway-pcache-client-gae/src/main/java/com/clouway/api/pcache/extensions/gae/GaeCacheManagerFactory.java
+++ clouway-pcache-client-gae/src/main/java/com/clouway/api/pcache/extensions/gae/GaeCacheManagerFactory.java
@@ -1,6 +1,8 @@
 package com.clouway.api.pcache.extensions.gae;
 
 import com.clouway.api.pcache.CacheManager;
+import com.google.appengine.api.memcache.ErrorHandlers;
+import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
 
 /**
@@ -9,7 +11,9 @@ import com.google.appengine.api.memcache.MemcacheServiceFactory;
 public final class GaeCacheManagerFactory {
 
   public static CacheManager create() {
-    return new GAECacheManager(MemcacheServiceFactory.getMemcacheService(), new GAECacheExceptionTranslator());
+    MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
+    memcacheService.setErrorHandler(ErrorHandlers.getDefault());
+    return new GAECacheManager(memcacheService, new GAECacheExceptionTranslator());
   }
 }
 
